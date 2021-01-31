70306dc0127212e9da8a72216c08f2409873422b
Ceren Turgut <veyselceren25@gmail.com>
2021-01-31 19:19:50 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        List<Car> GetAll();
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
+        List<Car> GetById(int ıd);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car {Id=1,ColorId=2,BrandId=3,ModelYear=1994,DailyPrice=100,Description="Dogan Slx" },
+                new Car {Id=2,ColorId=4,BrandId=4,ModelYear=2001,DailyPrice=500,Description="Toyota Corolla" },
+                new Car {Id=3,ColorId=5,BrandId=6,ModelYear=2007,DailyPrice=600,Description="Honda Civic" },
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int ıd)
+        {
+            return _cars.Where(c=>c.Id==ıd).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
